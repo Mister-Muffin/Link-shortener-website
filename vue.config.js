94bda54b9a87f481e02460b609280b3e99ddde72
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-11-28 22:27:10 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://mr-pine.de/links/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/'
 }
