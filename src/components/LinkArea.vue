5696fc3c573960d150d65dffc3e0a635bf32f1c7
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2022-01-02 00:18:59 +0100

    now https:// as standard when no target present

--- src/components/LinkArea.vue
+++ src/components/LinkArea.vue
@@ -2,10 +2,10 @@
   <v-container class="pa-0">
     <v-col class="pa-0">
       <link-row
-        v-for="(link) in links"
+        v-for="link in links"
         v-bind:key="link.short"
         :short="link.short"
-        :target="link.target"
+        :target="link.target ? link.target : 'https://'"
         :isNew="link.new"
         @addLink="addLink"
         @deleteLink="deleteLink"
